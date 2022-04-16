4516dc80318c360553885ef0cc7971bd173ed0ee
dkrkv1107 <aanchalkumari2744@gmail.com>
2022-04-16 08:43:09 +0530

    Added food menu adding function

---
+++ cafe/resources/views/admin/adminhome.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+</x-app-layout>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  @include("admin.admincss")
+  </head>
+  <body>
+   
+  <div class="container-scroller">ß
+    @include("admin.navbar")
+
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    @include("admin.adminscripts")
+
+  </body>
+</html>
---
+++ cafe/resources/views/food.blade.php
@@ -0,0 +1,92 @@
+<!-- ***** Menu Area Starts ***** -->
+<section class="section" id="menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="section-heading">
+                        <h6>Our Menu</h6>
+                        <h2>Try our famous delicacies with quality taste</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="menu-item-carousel">
+            <div class="col-lg-12">
+                <div class="owl-menu-item owl-carousel">
+                    <div class="item">
+                        <div class='card card1'>
+                            <div class="price"><h6>60 Rs.</h6></div>
+                            <div class='info'>
+                              <h1 class='title'>Cappachino Coffee</h1>
+                              <p class='description'>A simple coffe, but a settling taste to calm you down, as well as warm you inside.</p>
+                              <div class="main-text-button">
+                                  <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class='card card2'>
+                            <div class="price"><h6>72 Rs.</h6></div>
+                            <div class='info'>
+                              <h1 class='title'>Honey Pancake</h1>
+                              <p class='description'>A perfect blend of sweet and healthy, the honey pancake is a must to try.</p>
+                              <div class="main-text-button">
+                                  <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class='card card3'>
+                            <div class="price"><h6>40 Rs.</h6></div>
+                            <div class='info'>
+                              <h1 class='title'>Tall Mojito</h1>
+                              <p class='description'>Cold and never-wracking drink to chill you and your friends taste.</p>
+                              <div class="main-text-button">
+                                  <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class='card card4'>
+                            <div class="price"><h6>80 Rs.</h6></div>
+                            <div class='info'>
+                              <h1 class='title'>Blueberry CheeseCake</h1>
+                              <p class='description'>Cheesecakes have always been treated as a top class desset, and we serve the finest once.</p>
+                              <div class="main-text-button">
+                                  <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class='card card5'>
+                            <div class="price"><h6>65 Rs.</h6></div>
+                            <div class='info'>
+                              <h1 class='title'>Haka Noodles</h1>
+                              <p class='description'>Spicy and perfect mouth-watering noooooodles to watch up to your cravings and hunger.</p>
+                              <div class="main-text-button">
+                                  <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class='card card3'>
+                            <div class="price"><h6>75 Rs.</h6></div>
+                            <div class='info'>
+                              <h1 class='title'>Sour Mojito</h1>
+                              <p class='description'>Cold and never-wracking drink to chill you and your friends taste.</p>
+                              <div class="main-text-button">
+                                  <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Menu Area Ends ***** -->
